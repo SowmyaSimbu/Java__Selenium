cf8c0a7f6b9e6665d2d6f92ac82389438881d823
Sowmyasimbu <Sowmyausha93@gmail.com>
2025-05-28 19:31:35 +0530

    Adding first assistment to Sowmyabranch

---
+++ src/Assistment1_DataTypes.java
@@ -0,0 +1,22 @@
+public class Assistment1_DataTypes {
+
+    public static void main(String[] args) {
+        String Name = "Sowmya";
+        int RollNumber = 1223;
+        byte Age = 5;
+        char Grade = 'A';
+        boolean IsEnrolled = true;
+        float Feepaid =1500.50f;
+
+        System.out.println("Name of the Student : " +Name);
+        System.out.println("Name of the RollNumber : " +RollNumber);
+        System.out.println("Name of the Age : " +Age);
+        System.out.println("Name of the Grade : " +Grade);
+        System.out.println("Name of the IsEnrolled : " +IsEnrolled);
+        System.out.println("Name of the Feepaid : " +Feepaid);
+
+
+
+
+    }
+}
